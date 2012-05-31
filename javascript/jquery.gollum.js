7d41898f841778454edd9be64dfd3f1628ce4331
Kristof Marien <kristof.marien@litus.cc>
2012-05-31 13:30:47 +0200

    Gollum help works again

--- javascript/jquery.gollum.js
+++ javascript/jquery.gollum.js
@@ -78,6 +78,8 @@
 	            $(this).addClass('disabled');
 	        }
 	    });
+	    
+	    _defineHelp(functionbar.find('a.function-help'));
     };
     
     _functionButtonClick = function (e) {
@@ -86,7 +88,7 @@
         if (typeof def == 'object') {
             _executeAction($(this).closest('.gollum-editor').find('.gollum-editor-body'), def);
         }
-    }
+    };
     
     _executeAction = function ($that, definitionObject) {
         var txt = $that.val();
@@ -132,7 +134,7 @@
         
         if (repText)
             _replaceFieldSelection($that, repText, reselect, cursor);
-    }
+    };
     
     _getFieldSelectionPosition = function ($field) {
         if ($field.length) {
@@ -178,7 +180,7 @@
                 end: end
             };
         }
-    }
+    };
     
     _getFieldSelection = function ($field) {
         var selStr = '';
@@ -190,7 +192,7 @@
             return selStr;
         }
         return false;
-    }
+    };
     
     _replaceFieldSelection = function ($field, replaceText, reselect, cursorOffset) {
         var selPos = _getFieldSelectionPosition($field);
@@ -228,12 +230,133 @@
         
         if (scrollTop)
             $field[0].scrollTop = scrollTop;
-    }
+    };
     
     _getDefinitionFor = function (attr) {
         if (LanguageDefinition[attr] && typeof LanguageDefinition[attr] == 'object')
             return LanguageDefinition[attr];
         
         return null;
+    };
+    
+    _defineHelp = function($that) {
+        $that.removeClass('disabled')
+            .unbind('click')
+            .click(_helpButtonClick);
+        
+        _generateHelpMenu($that.closest('.gollum-editor').find('.gollum-editor-help'));
+    };
+    
+    _generateHelpMenu = function($that) {
+        $that.find('.gollum-editor-help-parent').html('');
+        $that.find('.gollum-editor-help-list').html('');
+        $that.find('.gollum-editor-help-content').html('');
+        
+        for (var i=0 ; i < LanguageDefinitionHelp.length ; i++) {
+            if (typeof LanguageDefinitionHelp[i] != 'object')
+                break;
+        
+            var $newLi = $('<li><a href="#" rel="' + i + '">' +
+            LanguageDefinitionHelp[i].menuName + '</a></li>');
+            $that.find('.gollum-editor-help-parent').append($newLi);
+            if (i === 0)
+                $newLi.children('a').addClass('selected');
+            
+            $newLi.children('a').click(_helpParentMenuClick);
+        }
+        
+        _generateHelpSubMenu($that, LanguageDefinitionHelp[0], 0);
+        $($that.find('.gollum-editor-help-list li a').get(0)).click();
+    };
+    
+    _generateHelpSubMenu = function($that, subData, index) {
+        $that.find('.gollum-editor-help-list').html('');
+        $that.find('.gollum-editor-help-content').html('');
+        for (var i=0 ; i < subData.content.length ; i++) {
+            if (typeof subData.content[i] != 'object')
+                break;
+            
+            var $subLi = $('<li><a href="#" rel="' + index + ':' + i + '">' +
+            subData.content[i].menuName + '</a></li>');
+            
+            $that.find('.gollum-editor-help-list').append($subLi);
+            $subLi.children('a').click(_helpSubMenuClick);
+        }
+    };
+    
+    _hideHelp = function($that) {
+        if ($.browser.msie) {
+            $that.css('display', 'none');
+        } else {
+            $that.animate(
+                {
+                    opacity: 0
+                },
+                200,
+                function() {
+                    $that.animate({ height: 'hide' }, 200);
+                }
+            );
+        }
+    };
+    
+    _showHelp = function() {
+        if ($.browser.msie) {
+            $that.css('display', 'block');
+        } else {
+            $that.animate(
+                {
+                    height: 'show'
+                },
+                200,
+                function() {
+                    $that.animate({ opacity: 1 }, 300);
+                }
+            );
+        }
+    };
+    
+    _helpButtonClick = function(e) {
+        e.preventDefault();
+        $that = $(e.target).closest('.gollum-editor').find('.gollum-editor-help');
+        if ($that.is(':visible')) {
+            _hideHelp($that);
+        } else {
+            _showHelp($that);
+        }
+    };
+    
+    _helpParentMenuClick = function(e) {
+        e.preventDefault();
+        $that = $(e.target).closest('.gollum-editor').find('.gollum-editor-help');
+
+        if ($(this).hasClass('selected'))
+            return;
+        
+        var helpIndex = $(this).attr('rel');
+        var subData = LanguageDefinitionHelp[helpIndex];
+        
+        $that.find('.gollum-editor-help-parent li a').removeClass('selected');
+        $(this).addClass('selected');
+        _generateHelpSubMenu($that, subData, helpIndex);
+        $($that.find('.gollum-editor-help-list li a').get(0)).click();
+    };
+    
+    _helpSubMenuClick = function(e) {
+        e.preventDefault();
+        $that = $(e.target).closest('.gollum-editor').find('.gollum-editor-help');
+
+        if ($(this).hasClass('selected'))
+            return;
+        
+        var rawIndex = $(this).attr('rel').split(':');
+        $that.find('.gollum-editor-help-list li a').removeClass('selected');
+        $(this).addClass('selected');
+        _showHelpFor($that, rawIndex[0], rawIndex[1]);
+    };
+    
+    _showHelpFor = function($that, index1, index2) {
+      var html = LanguageDefinitionHelp[index1].content[index2].data;
+      $that.find('.gollum-editor-help-content').html(html);
     };
 }) (jQuery)
